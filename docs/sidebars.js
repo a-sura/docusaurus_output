e01b17298f6a884886ccf39ea0abd49f9a17be8c
a-sura <abhishek.sura@gmail.com>
2026-02-23 15:51:23 +0530

    docs: update sidebars.js via MintDoc

---
+++ docs/sidebars.js
@@ -0,0 +1,15 @@
+
+module.exports = {
+  tutorialSidebar: [
+    {
+      type: 'category',
+      label: 'Guides',
+      items: ["intro","how-to-publish-to-docu"],
+    },
+    {
+      type: 'link',
+      label: 'OpenAPI Spec',
+      href: '/openapi.json',
+    },
+  ],
+};
